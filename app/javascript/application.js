3b2c679a35a8813073c3e9f748268088ab8712b9
Attila Juhász <goraffe@gmail.com>
2022-02-28 16:39:55 +0100

    Profile deletion gets confirm

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,26 @@ let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+// confirm for forms
+document.addEventListener("turbo:submit-start", (event) => {
+  confirmSubmission(event).then(() => {
+    // show progress bar and set submit state here.
+    // this block is optional.
+  })
+})
+
+function confirmSubmission(event) {
+  const button = event.target.querySelector("[data-confirm]")
+  const message = button?.dataset?.confirm
+
+  return new Promise(resolve => {
+    if (!message || confirm(message)) {
+      resolve()
+    } else {
+      event.detail.formSubmission.stop()
+      event.preventDefault()
+      event.stopImmediatePropagation()
+    }
+  })
+}
